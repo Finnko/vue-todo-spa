3ac1d4298241934974efb603376d4840e6a52daa
Alex Khab <finko85@gmail.com>
2019-10-27 22:19:09 +0300

    add tasks list component

--- src/store/index.js
+++ src/store/index.js
@@ -19,6 +19,7 @@ export default new Vuex.Store({
       commit('createTask', task)
     }
   },
-  modules: {
+  getters: {
+    tasks: state => state.tasks
   }
 });
--- src/views/List.vue
+++ src/views/List.vue
@@ -3,13 +3,65 @@
     <h1>
       List
     </h1>
+    <table v-if="tasks.length">
+      <thead>
+      <tr>
+        <th>#</th>
+        <th>Title</th>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Status</th>
+        <th>View Task</th>
+      </tr>
+      </thead>
+      <tbody>
+        <tr
+            v-for="(task, idx) of tasks"
+            :key="task.id"
+        >
+          <td>{{idx + 1}}</td>
+          <td>{{task.title}}</td>
+          <td>{{new Date(task.date).toLocaleDateString()}}</td>
+          <td class="task-desc">
+            <p class="task-desc__text">
+              {{task.description}}
+            </p>
+          </td>
+          <td>{{task.status}}</td>
+          <td>
+            <router-link tag="button" class="btn btn-small" :to="'/task/' + task.id">
+              View
+            </router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-else>
+      No avaliable tasks
+    </p>
   </div>
+
 </template>
 
 <script>
-    export default {}
+    export default {
+      computed: {
+          tasks() {
+              return this.$store.getters.tasks
+          }
+      }
+    }
 </script>
 
 <style scoped>
+  .task-desc {
+    max-width: 375px;
+  }
 
+  .task-desc__text {
+    margin: 0;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow-x: hidden;
+  }
 </style>
